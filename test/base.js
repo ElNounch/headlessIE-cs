ec6f50c1e63cf5800caa9fe4ea6b1b1918c7ca7f
ElNounch <ElNounch@users.noreply.github.com>
2015-05-10 18:54:25 +0200

    Appveyor

---
+++ test/base.js
@@ -0,0 +1,82 @@
+var test = require('tape')
+var express = require('express')
+var fs = require('fs')
+child_process = require('child_process')
+
+function createRandomLocalAddress() {
+    function upTo( max ) {
+        return Math.floor( Math.random() * (max + 1) )
+    }
+    return '127.' + upTo(255) + '.' + upTo(255) + '.' + (upTo(252) + 2)
+}
+
+test('versions test', function (t) {
+    t.plan(2)
+
+    child_process.execFile( 'bin/headlessIE.exe', [ '--version' ], function VersionRunner( err, stdout ) {
+        if( /^(\d+)(\.(\d+)){0,3}/.test( stdout ) ) {
+            t.pass( 'got assembly version : "' + stdout + '"' )
+        }
+    })
+
+    child_process.execFile( 'bin/headlessIE.exe', [ '--ie-version' ], function VersionRunner( err, stdout ) {
+        if( /^(\d+)(\.(\d+)){0,3}/.test( stdout ) ) {
+            t.pass( 'got IE version : "' + stdout + '"' )
+        }
+    })
+})
+
+test('functionnal test', { timeout: 30000 }, function (t) {
+    var app = express()
+    var server
+    var proc
+    var timer
+    var job_done = false
+    var local_addr = createRandomLocalAddress()
+
+    app.get('/entrance', function (req, res) {
+        res.setHeader( 'Connection', 'close' )
+        res.setHeader( 'Content-Type', 'text/html' )
+        res.send( '<html><head><script>location.href="/javascript_passed"</script><body></body></html>' )
+        res.end()
+        t.pass( 'entrance page accessed' )
+    })
+    app.get('/javascript_passed', function (req, res) {
+        res.setHeader( 'Connection', 'close' )
+        res.setHeader( 'Content-Type', 'text/plain' )
+        res.send( 'Job done !' )
+        res.end()
+        t.pass( 'javascript worked' )
+        job_done = true
+        clearTimeout( timer )
+        proc.kill()//'SIGHUP')
+        server.close()
+    })
+
+    server = app.listen(8000, local_addr)
+
+    timer = setTimeout( function onTimeOut() {
+        proc.kill('SIGHUP')
+        server.close()
+    }, 25000)
+
+    proc = child_process.spawn( 'bin/headlessIE.exe', [ 'http://' + local_addr + ':8000/entrance' ] )
+
+    proc.stdout.on( 'data', function onStdout( data ) {
+        t.comment( 'console message : ' + data )
+    })
+
+    proc.stderr.on( 'data', function onStderr( data ) {
+        t.comment( 'error message : ' + data )
+    })
+
+    proc.on('exit', function onIEExit( code, signal ) {
+        if( job_done ) {
+            t.ok( code === null, 'exit code is correct' )
+            t.ok( signal === 'SIGTERM', 'killed by signal' )
+        } else {
+            t.fail( 'unexpected browser quit' )
+        }
+        t.end()
+    })
+})
