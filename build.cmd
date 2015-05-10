0375c09bb6c6e023cb78bfb91e94a463305f426c
ElNounch <ElNounch@users.noreply.github.com>
2015-05-10 20:28:49 +0200

    csproj

--- build.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-del /S /Q bin > NUL:
-mkdir bin 2> NUL:
-csc /noconfig /r:System.Dll /r:System.Windows.Forms.Dll /out:bin\headlessIE.exe /t:exe src\headlessIE.cs
--- test_setup.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-npm install -g npm
-mkdir node_modules 2> NUL:
-npm install tape express tap-spec
