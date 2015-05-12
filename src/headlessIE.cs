c32a124eb320fc01d5f41aa1111561419ae8d3e3
ElNounch <ElNounch@users.noreply.github.com>
2015-05-13 00:25:14 +0200

    Versions data

--- src/headlessIE.cs
+++ src/headlessIE.cs
@@ -28,6 +28,10 @@ using System;
 using System.Windows.Forms;
 using System.Reflection;
 
+[assembly:AssemblyVersion("0.1.0")]
+[assembly:AssemblyFileVersion("0.1.0")]
+[assembly:AssemblyInformationalVersion("0.1.0")]
+
 namespace HeadlessIE
 {
 	public class Entrance
